fa5a1a056e2114d58760b01f67467c37af811346
quybot <quyp@pm.me>
2019-09-23 12:53:55 -0400

    update

--- Employee-Database/employeedb.sql
+++ Employee-Database/employeedb.sql
@@ -42,14 +42,14 @@ CREATE TABLE "titles" (
     "to_date" DATE NOT NULL
  );
 
-ALTER TABLE employees
+ALTER TABLE dept_emp
 ADD FOREIGN KEY(dept_no)
 REFERENCES departments(dept_no)
 ON DELETE SET NULL;
 
 ALTER TABLE dept_manager
-ADD FOREIGN KEY(emp_no)
-REFERENCES employees(emp_no)
+ADD FOREIGN KEY(dept_no)
+REFERENCES departments(dept_no)
 ON DELETE SET NULL;
 
 --------------------------------------------------
